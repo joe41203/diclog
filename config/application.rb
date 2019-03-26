d889aa99709585b1a66eb7e1a79ada08b4d69c8b
joe <kinjoyuji@gmail.com>
2019-03-26 14:11:32 +1100

    setting generator

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,12 @@ module Diclog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.factory_bot dir: 'spec/factories'
+      g.factory_bot true
+      g.test_framework :rspec, view_specs: false, helper_specs: false, controller_specs: false, routing_specs: false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -29,10 +35,5 @@ module Diclog
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    config.generators do |g|
-      g.factory_bot dir: 'spec/factories'
-      g.factory_bot true
-    end
   end
 end
